3f228d768c70bb946984ef320700858941d747ff
Naohisa Minagawa <info@clay-system.jp>
2013-09-03 05:40:07 +0900

    ツイートの登録処理を微調整

--- batch/SetupTweets.php
+++ batch/SetupTweets.php
@@ -18,21 +18,61 @@ $_SERVER["REQUEST_URI"] = "/batch/ImportTweets.php";
 require_once(dirname(__FILE__)."/../require.php");
 
 $connection = new Connection();
-$sql = "SELECT keywords.*, accounts.account_id";
-$sql .= " FROM keywords, account_groups, accounts";
-$sql .= " WHERE keywords.keyword_id = account_groups.keyword_id";
-$sql .= " AND account_groups.account_group_id = accounts.account_group_id";
-$sql .= " AND keywords.delete_flg = 0";
+$sql = "SELECT accounts.*, account_groups.keyword_id, account_groups.pickup_limit, account_groups.pickup_count FROM accounts, account_groups";
+$sql .= " FROM accounts, account_groups";
+$sql .= " WHERE account_groups.account_group_id = accounts.account_group_id";
+$sql .= " AND account_groups.import_flag = 1";
 $result = $connection->query($sql);
-$keywords = $result->fetchAll();
+$accounts = $result->fetchAll();
 $result->close();
-if(is_array($keywords)){
-	foreach($keywords as $keyword){
-		// そのアカウントのmax_idを取得します。
-		$sql = "SELECT * FROM tweet_search_cache WHERE account_id = '".$connection->escape($keyword["account_id"])."'";
+if(is_array($accounts)){
+	foreach($accounts as $account){
+		// そのアカウントに投稿する投稿を取得します。
+		$sql = "SELECT * FROM tweet_caches WHERE keyword_id = '".$connection->escape($account["keyword_id"])."' AND delete_flg = 0 ORDER BY retweet_count DESC LIMIT ".$connection->escape($account["pickup_count"]);
 		$result = $connection->query($sql);
-		$search_caches = $result->fetchAll();
+		$cached_tweets = $result->fetchAll();
 		$result->close();
+		
+		foreach($cached_tweets as $cached_tweet){
+			// そのアカウントの予約済みツイート数を取得します。
+			$sql = "SELECT * FROM tweets WHERE account_id = '".$connection->escape($account["account_id"])."' AND post_status = 1 AND delete_flg = 0";
+			$result = $connection->query($sql);
+			$tweets = $result->fetchAll();
+			$result->close();
+			
+			// 予約済みツイートが規定数以上なら登録終了します。
+			if(is_array($tweets) && $account["pickup_count"] <= count($tweets)) break;
+			
+			// ツイートを登録します。
+			$sqlval = array();
+			$sqlval["account_id"] = $account["account_id"];
+			$sqlval["source_post_id"] = $cached_tweet["post_id"];
+			$sqlval["source_favorite_count"] = $cached_tweet["favorite_count"];
+			$sqlval["source_retweet_count"] = $cached_tweet["retweet_count"];
+			$sql = "SELECT * FROM tweets WHERE account_id = '".$connection->escape($sqlval["account_id"])."' AND  source_post_id = '".$connection->escape($sqlval["source_post_id"])."'";
+			$result = $connection->query($sql);
+			$registeredTweets = $result->fetchAll();
+			$result->close();
+			if(is_array($registeredTweets) && count($registeredTweets) > 0){
+				if($registeredTweets[0]["post_status"] == "1"){
+					foreach($sqlval as $key => $value){
+						$sqlval[$key] = $key." = '".$connection->escape($value)."'";
+					}
+					$sql = "UPDATE tweets SET ".implode(", ", $sqlval);
+					$sql .= " WHERE tweet_id = '".$connection->escape($registeredTweets[0]["tweet_id"])."'";
+					$result = $connection->query($sql);
+				}
+			}else{
+				$sqlval["tweet_text"] = $cached_tweet["tweet_text"];
+				$sqlval["rank"] = mt_rand(1, 100000);
+				foreach($sqlval as $key => $value){
+					$sqlval[$key] = $connection->escape($value);
+				}
+				$sql = "INSERT INTO tweets";
+				$sql .= "(".implode(", ", array_keys($sqlval)).") VALUES ('".implode("', '", $sqlval)."')";
+				$result = $connection->query($sql);
+			}
+				
 		$max_id = "";
 		if(is_array($search_caches) && count($search_caches) > 0){
 			$max_id = $search_caches[0]["max_id"];
