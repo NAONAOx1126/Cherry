f8895197d924300283cd3fa4639003511141dc0c
Naohisa Minagawa <info@clay-system.jp>
2013-09-04 17:10:48 +0900

    出力メッセージを削除

--- batch/UpdateStatus.php
+++ batch/UpdateStatus.php
@@ -28,7 +28,6 @@ if(is_array($accounts)){
 		$condition = array("user_id" => $account["user_id"], "count" => 200, "trim_user" => false, "exclude_replies" => true, "include_rts" => false);
 		$tweets = (array) $twitter->statuses_userTimeline($condition);
 		if(is_array($tweets) && count($tweets) > 0){
-			print_r($tweet);
 			foreach($tweets as $tweet){
 				if($tweet->favorite_count || $tweet->retweet_count){
 					$sql = "UPDATE tweets SET favorite_count = '".$connection->escape($tweet->favorite_count)."', retweet_count = '".$connection->escape($tweet->retweet_count)."' WHERE post_id = '".$connection->escape($tweet->id)."'";
