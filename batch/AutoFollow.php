435384cc201220be82644f1a5be79b9b8552b43f
Naohisa Minagawa <info@clay-system.jp>
2013-10-04 20:49:35 +0900

    自動フォローの処理を更新

--- batch/AutoFollow.php
+++ batch/AutoFollow.php
@@ -32,22 +32,43 @@ if(is_array($accounts)){
 		$twitter = getTwitter($account["account_id"]);
 	    
 	    // 次のフォローを取得
-	    $sql = "SELECT * FROM follow_caches WHERE account_id = '".$account["account_id"]."'";
+	    $sql = "SELECT * FROM follower_caches WHERE account_id = '".$account["account_id"]."'";
 	    $result = $connection->query($sql);
 	    $follow = $result->fetch();
 	    $result->close();
 	    
-	    // 取得したフォローのフォロワーを取得する。
-	    $followers = (array) $twitter->followers_ids(array("user_id" => $follow["user_id"], "count" => "1000"));
-	    unset($followers["httpstatus"]);
-	    print_r($followers);
-	    /*
-	    for($i = 0; $i < 5; $i ++){
-	        $index = mt_rand(0, count($followers));
-	        if($index < count($followers)){
-	            $follower
-	        }
+	    if(!empty($follow)){
+	        // 自分の情報を取得する。
+		    $me = $twitter->users_show(array("user_id" => $account["user_id"]));
+		    print_r($me);
+		    exit;
+	        // 取得したフォローのフォロワーを取得する。
+    	    $result = (array) $twitter->followers_ids(array("user_id" => $follow["user_id"], "count" => "1000"));
+    	    $followers = $result["ids"];
+    	    for($i = 0; $i < 5; $i ++){
+    	        $index = mt_rand(0, count($followers));
+    	        if($index < count($followers)){
+    	            $user_id = $followers[$index];
+    	            $user = $twitter->users_show(array("user_id" => $user_id));
+    	            if($_SESSION["ADMINISTRATOR"]["ignore_non_japanese_flg"] == "1" && mb_check_encoding($user->description, "ASCII")){
+    	                continue;
+    	            }
+    	            if($_SESSION["ADMINISTRATOR"]["ignore_bot_flg"] == "1" && preg_match("/bot/i", $user->description) > 0){
+    	                continue;
+    	            }
+    	            if($_SESSION["ADMINISTRATOR"]["ignore_url_flg"] == "1" && preg_match("/http:\\/\\//i", $user->description) > 0){
+    	                continue;
+    	            }
+    	            
+    	            $twitter->friendship_create()
+    	            $follow
+    	            
+    	            if($follow["depth"] < $_SESSION["ADMINISTRATOR"]["tree_depth"]){
+    	                $connection->query("INSERT IGNORE INTO follower_caches(account_id, user_id, depth) VALUES ('".$follow["account_id"]."', '".$user_id."', '".($follow["depth"] + 1)."')");
+    	            }
+    	        }
+    	    }
+    	    $connection->query("DELETE FROM follower_caches WHERE account_id = '".$follow["account_id"]."' AND user_id = '".$follow["user_id"]."'");
 	    }
-	    */
 	}
 }
