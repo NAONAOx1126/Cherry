6b872bba361201a29e4f9d4dcb5e100c2c5c3f8a
Naohisa Minagawa <info@clay-system.jp>
2013-10-07 16:19:32 +0900

    残す場合の出力もチェックできるように修正

--- batch/CleanupImage.php
+++ batch/CleanupImage.php
@@ -14,10 +14,16 @@ if ($handle = opendir(dirname(__FILE__)."/../images/")) {
         if(preg_match("/^([0-9]+)-([0-9]+)$/", $file, $params) > 0){
             $result = $connection->query("SELECT COUNT(*) AS count FROM tweets WHERE source_post_id = '".$params[1]."'");
             $data = $result->fetch();
-            if($data["count"] > 0) continue;
+            if($data["count"] > 0){
+                echo "REMAIN: ".$file."\r\n";
+                continue;
+            }
             $result = $connection->query("SELECT COUNT(*) AS count FROM tweet_caches WHERE post_id = '".$params[1]."'");
             $data = $result->fetch();
-            if($data["count"] > 0) continue;
+            if($data["count"] > 0){
+                echo "REMAIN: ".$file."\r\n";
+                continue;
+            }
             echo "DELETE : ".$file."\r\n";
         }
     }
