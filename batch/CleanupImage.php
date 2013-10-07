f6731ab5fbe3903214a6cd586ce6e2fd24d6666c
Naohisa Minagawa <info@clay-system.jp>
2013-10-07 16:26:48 +0900

    画像クリーンアップ処理を更新

--- batch/CleanupImage.php
+++ batch/CleanupImage.php
@@ -8,25 +8,30 @@ require_once(dirname(__FILE__)."/../require.php");
 
 $connection = new Connection();
 
-if ($handle = opendir(dirname(__FILE__)."/../images/")) {
-    /* ディレクトリをループする際の正しい方法です */
-    while (false !== ($file = readdir($handle))) {
-        if(preg_match("/^([0-9]+)-([0-9]+)$/", $file, $params) > 0){
-            $result = $connection->query("SELECT COUNT(*) AS count FROM tweets WHERE source_post_id = '".$params[1]."'");
-            $data = $result->fetch();
-            if($data["count"] > 0){
-                echo "REMAIN: ".$file."\r\n";
-                continue;
-            }
-            $result = $connection->query("SELECT COUNT(*) AS count FROM tweet_caches WHERE post_id = '".$params[1]."'");
-            $data = $result->fetch();
-            if($data["count"] > 0){
-                echo "REMAIN: ".$file."\r\n";
-                continue;
-            }
-            echo "DELETE : ".$file."\r\n";
-        }
-    }
+$dirs = array("images", "thumbnails");
 
-    closedir($handle);
+foreach($dirs as $dir){
+    if ($handle = opendir(dirname(__FILE__)."/../".$dir."/")) {
+        /* ディレクトリをループする際の正しい方法です */
+        while (false !== ($file = readdir($handle))) {
+            if(preg_match("/^([0-9]+)-([0-9]+)$/", $file, $params) > 0){
+                $result = $connection->query("SELECT COUNT(*) AS count FROM tweets WHERE source_post_id = '".$params[1]."'");
+                $data = $result->fetch();
+                if($data["count"] > 0){
+                    // echo "REMAIN : ".$file."\r\n";
+                    continue;
+                }
+                $result = $connection->query("SELECT COUNT(*) AS count FROM tweet_caches WHERE post_id = '".$params[1]."'");
+                $data = $result->fetch();
+                if($data["count"] > 0){
+                    // echo "REMAIN : ".$file."\r\n";
+                    continue;
+                }
+                // echo "DELETE : ".$file."\r\n";
+                unlink(dirname(__FILE__)."/../".$dir."/".$file);
+            }
+        }
+    
+        closedir($handle);
+    }
 }
